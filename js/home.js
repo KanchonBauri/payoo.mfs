c0a74f3df1323b02e749bf5e24c27e5b3864a25f
KanchonBauri <kanchonbruei1998@gmail.com>
2024-09-19 14:02:48 +0600

    add amount

--- js/home.js
+++ js/home.js
@@ -1,1 +1,26 @@
-console.log('hello')
+
+
+// step 1
+document.getElementById('btn-process').addEventListener('click', function (event) {
+    // step 2
+    event.preventDefault();
+    // console.log('hhhfh');
+    // step 3
+    const inputAmount = document.getElementById('input-amount').value;
+    const inputPassword = document.getElementById('input-password').value;
+    // console.log(inputAmount, inputPassword)
+
+    // step 4
+    if (inputPassword === '123') {
+        // step 5
+        const availableBalance = document.getElementById('available-balance').innerText;
+        // step 6
+        const newAmount = parseInt(availableBalance) + parseInt(inputAmount);
+        console.log(newAmount);
+
+        document.getElementById('available-balance').innerText = newAmount;
+
+    }
+    document.getElementById('input-amount').value = '';
+    document.getElementById('input-password').value = '';
+})
